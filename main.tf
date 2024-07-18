aea135d6914faacb7d89e1f65d263a18a3817ea3
sajangupta74 <sajangupta74@gmail.com>
2024-07-18 16:58:39 +0530

    Initial commit

---
+++ main.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+  region  = var.region
+  profile = var.profile
+}
+
+module "eks-jx" {
+  source                               = "github.com/jenkins-x/terraform-aws-eks-jx?ref=v2.0.0"
+  cluster_version                      = var.cluster_version
+  cluster_name                         = var.cluster_name
+  region                               = var.region
+  profile                              = var.profile
+  jx_git_url                           = var.jx_git_url
+  jx_bot_username                      = var.jx_bot_username
+  jx_bot_token                         = var.jx_bot_token
+  force_destroy                        = var.force_destroy
+  nginx_chart_version                  = var.nginx_chart_version
+  install_kuberhealthy                 = var.install_kuberhealthy
+  enable_worker_group                  = false
+  enable_worker_groups_launch_template = true
+  create_addon_role                    = true
+  enable_ebs_addon                     = true
+}
---
+++ variables.tf
@@ -0,0 +1,56 @@
+// ----------------------------------------------------------------------------
+// Optional Variables
+// ----------------------------------------------------------------------------
+variable "region" {
+  description = "AWS region code for creating resources."
+  type        = string
+}
+
+variable "profile" {
+  description = "Profile stored in aws config or credentials file"
+  type        = string
+}
+
+variable "cluster_version" {
+  description = "Kubernetes version to use for the EKS cluster."
+  type        = string
+}
+
+variable "cluster_name" {
+  description = "Name of the Kubernetes cluster to create"
+  type        = string
+  default     = ""
+}
+
+variable "force_destroy" {
+  description = "Flag to determine whether storage buckets get forcefully destroyed. If set to false, empty the bucket first in the aws s3 console, else terraform destroy will fail with BucketNotEmpty error"
+  type        = bool
+  default     = false
+}
+
+variable "jx_git_url" {
+  description = "URL for the Jenins X cluster git repository"
+  type        = string
+}
+
+variable "jx_bot_username" {
+  description = "Bot username used to interact with the Jenkins X cluster git repository"
+  type        = string
+}
+
+variable "jx_bot_token" {
+  description = "Bot token used to interact with the Jenkins X cluster git repository"
+  type        = string
+}
+
+variable "nginx_chart_version" {
+  type        = string
+  description = "nginx chart version"
+  default     = "3.12.0"
+}
+
+variable "install_kuberhealthy" {
+  description = "Flag to specify if kuberhealthy operator should be installed"
+  type        = bool
+  default     = false
+}
